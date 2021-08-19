dd5de93c299e7933f3190b5af5b67be21eda417e
Trần Hiếu <83171071+dacvuips@users.noreply.github.com>
2021-08-19 15:24:37 +0700

    Update main.js

--- food-dashboard/app/models/v2/main.js
+++ food-dashboard/app/models/v2/main.js
@@ -316,7 +316,7 @@ getEle("page").addEventListener('click', (event) => {
 
 getEle("search").addEventListener('keyup', async() => {
 
-    let type = getEle("search").value.trim().toLowerCase();
+    let type = xoa_dau(getEle("search").value.trim().toLowerCase());
 
 
     let search = [];
@@ -325,7 +325,9 @@ getEle("search").addEventListener('keyup', async() => {
 
 
         let searchs = reps.data.filter((value) => {
-            if ((value.tenMon.trim().toLowerCase().indexOf(type) !== -1) || (value.moTa.trim().toLowerCase().indexOf(type) !== -1)) {
+            let TM = xoa_dau(value.tenMon)
+            let MT = xoa_dau(value.moTa)
+            if ((TM.trim().toLowerCase().indexOf(type) !== -1) || (MT.trim().toLowerCase().indexOf(type) !== -1)) {
                 return value
 
             }
@@ -341,3 +343,24 @@ getEle("search").addEventListener('keyup', async() => {
     }
 
 })
+
+const xoa_dau = (str) => {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, " ");
+    str = str.replace(/\s+/g, ' ');
+    str = str.replace(' ', '');
+    return str;
+}
